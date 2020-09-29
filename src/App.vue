e7757c15b05dd8f4ae2978aeb8154b11e3b3bc78
Jonathan Nakache <john.nakache@gmail.com>
2020-09-29 19:55:58 +0300

    adding a new task button

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,71 @@
 <template>
-  <div id="app" class="container">
-    <h1>TODO APP</h1>
-    <b-card-group deck>
-      <b-card column header="To do" text-variant="success">
-        <draggable class="list-group" :list="todos" group="todos">
-          <b-list-group-item v-for="item in todos" :key="item.id">{{
-            item.title
-          }}</b-list-group-item>
-        </draggable>
-      </b-card>
-      <b-card column header="In progress">
-        <draggable class="list-group" :list="inProgress" group="todos">
-          <b-list-group-item v-for="item in inProgress" :key="item.id">{{
-            item.title
-          }}</b-list-group-item>
-        </draggable>
-      </b-card>
-      <b-card column header="Done">
-        <draggable class="list-group" :list="done" group="todos">
-          <b-list-group-item v-for="item in done" :key="item.id">{{
-            item.title
-          }}</b-list-group-item>
-        </draggable>
-      </b-card>
-    </b-card-group>
+  <div id="app" class="container-fluid">
+    <div class="d-flex flex-row-reverse mt-4">
+      <b-button v-b-modal.modal-new-task variant="danger">New Task</b-button>
+    </div>
+    <b-modal id="modal-new-task" title="Add a new todo" @ok="handleOk">
+      <b-form-input
+        @keyup.enter="handleOk"
+        type="text"
+        name="todo"
+        id="todo"
+        v-model="todo"
+      ></b-form-input>
+    </b-modal>
+
+    <div class="container">
+      <h1 class="my-4">TODO APP</h1>
+      <div class="row justify-content-between">
+        <div class="col-xl">
+          <div class="bg-col">
+            <h2>To do</h2>
+            <ul>
+              <draggable class="list-group" :list="todos" group="todos">
+                <li
+                  v-for="item in todos"
+                  :key="item.id"
+                  class="font-weight-bold"
+                >
+                  {{ item.title }}
+                </li>
+              </draggable>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xl">
+          <div class="bg-col">
+            <h2>In progress</h2>
+            <ul>
+              <draggable class="list-group" :list="inProgress" group="todos">
+                <li
+                  v-for="item in inProgress"
+                  :key="item.id"
+                  class="font-weight-bold"
+                >
+                  {{ item.title }}
+                </li>
+              </draggable>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xl">
+          <div class="bg-col">
+            <h2>Done</h2>
+            <ul>
+              <draggable class="list-group" :list="done" group="todos">
+                <li
+                  v-for="item in done"
+                  :key="item.id"
+                  class="font-weight-bold"
+                >
+                  {{ item.title }}
+                </li>
+              </draggable>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +80,7 @@ export default {
   },
   data() {
     return {
+      createTodo: false,
       todo: "",
       todos: [
         {
@@ -59,16 +102,64 @@ export default {
       ],
     };
   },
+  methods: {
+    handleOk() {
+      if (this.todo.trim() !== "") {
+        this.todos.push({
+          title: this.todo,
+          id: uuidv4(),
+        });
+      }
+      this.todo = "";
+    },
+  },
 };
 </script>
 
 <style>
+body {
+  min-height: 100vh;
+  background-image: linear-gradient(
+    to right top,
+    #7060b8,
+    #6f67c2,
+    #6d6ecd,
+    #6b75d7,
+    #677ce2
+  );
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  box-sizing: border-box;
+}
+.bg-col {
+  background: #f2f2f2;
+  border-radius: 10px;
+  margin: 20px;
+  min-height: 15 0px;
+}
+h1 {
+  color: #fff;
+}
+h2 {
+  padding: 30px 0;
+  font-weight: bold;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+ul li {
+  width: 90%;
+  padding: 10px;
+  margin: 10px auto;
+  border: 1px solid #000;
+  border-radius: 5px;
+  background: #fff;
 }
 </style>
