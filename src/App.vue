9a2eea71abef84c4c17fdbdc76331c77a81b2159
Jonathan Nakache <john.nakache@gmail.com>
2020-09-29 17:34:41 +0300

    init + draggable

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>TODO APP</h1>
+    <b-card-group deck>
+      <b-card column header="To do" text-variant="success">
+        <draggable class="list-group" :list="todos" group="todos">
+          <b-list-group-item v-for="item in todos" :key="item.id">{{
+            item.title
+          }}</b-list-group-item>
+        </draggable>
+      </b-card>
+      <b-card column header="In progress">
+        <draggable class="list-group" :list="inProgress" group="todos">
+          <b-list-group-item v-for="item in inProgress" :key="item.id">{{
+            item.title
+          }}</b-list-group-item>
+        </draggable>
+      </b-card>
+      <b-card column header="Done">
+        <draggable class="list-group" :list="done" group="todos">
+          <b-list-group-item v-for="item in done" :key="item.id">{{
+            item.title
+          }}</b-list-group-item>
+        </draggable>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { v4 as uuidv4 } from "uuid";
+import draggable from "vuedraggable";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    draggable,
+  },
+  data() {
+    return {
+      todo: "",
+      todos: [
+        {
+          title: "todo",
+          id: uuidv4(),
+        },
+      ],
+      inProgress: [
+        {
+          title: "progress",
+          id: uuidv4(),
+        },
+      ],
+      done: [
+        {
+          title: "done",
+          id: uuidv4(),
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
